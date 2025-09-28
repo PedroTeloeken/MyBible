702871e2f0e4650f54a57b5f855abfe1ddecee62
pedroteloeken <pedroteloeken2211@gmail.com>
2025-09-28 10:44:28 -0300

    Initial Commit

---
+++ MyBible/MyBibleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBibleApp.swift
+//  MyBible
+//
+//  Created by Pedro Teloeken on 28/09/25.
+//
+
+import SwiftUI
+
+@main
+struct MyBibleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyBibleTests/MyBibleTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyBibleTests.swift
+//  MyBibleTests
+//
+//  Created by Pedro Teloeken on 28/09/25.
+//
+
+import Testing
+@testable import MyBible
+
+struct MyBibleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
