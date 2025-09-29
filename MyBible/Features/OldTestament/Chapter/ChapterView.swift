0ba8134061d69bd693f0787df9a22b6b50a88603
pedroteloeken <pedroteloeken2211@gmail.com>
2025-09-28 22:45:51 -0300

    visual da lista dos versiculos

--- MyBible/Features/OldTestament/Chapter/ChapterView.swift
+++ MyBible/Features/OldTestament/Chapter/ChapterView.swift
@@ -11,6 +11,8 @@ struct ChapterView: View {
     
     @StateObject var viewModel: ChapterViewModel
     
+    @State private var isExpandedMode = true
+    
     init(bookName: String, chapters: [Chapter]) {
         _viewModel = StateObject(
             wrappedValue: ChapterViewModel(bookName: bookName, chapters: chapters)
@@ -18,20 +20,67 @@ struct ChapterView: View {
     }
     
     var body: some View {
-        List {
+        NavigationStack {
+            List {
+                Picker("Modo de Visualização", selection: $isExpandedMode) {
+                    Text("Expandido").tag(true)
+                    Text("Por Capítulo").tag(false)
+                }
+                .pickerStyle(.segmented)
+                .listRowInsets(EdgeInsets())              
+                .listRowBackground(Color.clear)
+                
+                
+                if isExpandedMode {
+                    expandedView
+                } else {
+                    chapterListView
+                }
+            }
+            .navigationTitle(viewModel.bookName)
+            .navigationBarTitleDisplayMode(.large)
+        }
+    }
+    
+    private var expandedView: some View {
+        //Group {
             ForEach(viewModel.chapters, id: \.number) { chapter in
-                Section("Capítulo \(chapter.number.description)") {
+                Section("Capítulo \(chapter.number)") {
                     ForEach(chapter.verses, id: \.number) { verse in
                         (Text("\(verse.number) ")
                             .fontWeight(.semibold)
-                        + Text(verse.text))
-                            .font(.body)
+                         + Text(verse.text))
+                        .font(.body)
                     }
                 }
             }
-        }
-        .navigationTitle(viewModel.bookName)
+        //}
     }
+    
+    private var chapterListView: some View {
+        Section {
+            ForEach(viewModel.chapters, id: \.number) { chapter in
+                //NavigationLink(destination: ChapterDetailView(chapter: chapter)) {
+                HStack {
+                    Text("Capítulo \(chapter.number)")
+                        .font(.headline)
+                    
+                    Spacer()
+                    
+                    Text("\(chapter.verses.count) versículos")
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                    
+                    Image(systemName: "chevron.right")
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+                .padding(.vertical, 4)
+                //}
+            }
+        }
+    }
+    
 }
 
 
